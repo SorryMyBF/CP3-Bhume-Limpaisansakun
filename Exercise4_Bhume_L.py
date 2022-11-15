a861d6c733056751e4a1bc941266fa8d9145a815
SorryMyBF <117795081+SorryMyBF@users.noreply.github.com>
2022-11-15 09:17:23 +0700

    Add files via upload

---
+++ Exercise4_Bhume_L.py
@@ -0,0 +1,10 @@
+FE=89
+GB=73
+ICS=86
+CP=95
+print("----Your Score----")
+print("Foundation English :",FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming :",CP)
+print("------------------")
