a7131a84c1513c8d858a9698b6784b915e68f7e6
SorryMyBF <117795081+SorryMyBF@users.noreply.github.com>
2022-11-15 11:07:06 +0700

    Add files via upload

---
+++ Exercise5_1_Bhume_L.py
@@ -0,0 +1,6 @@
+A = input("Input first number : ")
+B = input("Input second number : ")
+print(A,"+",B,"=",int(A)+int(B))
+print(A,"-",B,"=",int(A)-int(B))
+print(A,"*",B,"=",int(A)*int(B))
+print(A,"/",B,"=",int(A)/int(B))
---
+++ Exercise5_2_Bhume_L..py
@@ -0,0 +1,4 @@
+Distance = int(input("Input Distance(Km.) : "))
+Time = int(input("Input Time(H.) : "))
+velocity = Distance/Time
+print("velocity = ",velocity,"Km/H")
