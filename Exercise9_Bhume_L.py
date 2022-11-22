1be48ebc9b5ed6f63f33d65e2f95e02d60f870cd
SorryMyBF <117795081+SorryMyBF@users.noreply.github.com>
2022-11-22 09:31:47 +0700

    Add files via upload

---
+++ Exercise9_Bhume_L.py
@@ -0,0 +1,7 @@
+usernameInput = input("username :")
+passwordInput = input("password :")
+while   usernameInput != "admin" or passwordInput != "1234":
+    print("-------------")
+    usernameInput = input("username :")
+    passwordInput = input("password :")
+print("connected")
